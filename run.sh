a181464200ab890b258831c17dc66517495c1572
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2023-08-29 19:53:02 +0200

    make usage clarification

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 if ! command -v make &> /dev/null
 then
     echo "make tool is missing"
+    echo "if you don't want to install it you can check content of the ./docker/Makefile and run the corresponding command manually"
     exit 1
 fi
 
