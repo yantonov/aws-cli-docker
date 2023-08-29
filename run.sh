8ae959a3524307d3a26651294d846e15b0ee85e2
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2023-08-29 19:52:08 +0200

    check if make is available

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+if ! command -v make &> /dev/null
+then
+    echo "make tool is missing"
+    exit 1
+fi
+
 export SRC="$(pwd)"
 
 cd "$(dirname "$0")/docker"
