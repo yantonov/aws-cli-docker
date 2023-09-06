fac13fb5d6ae9d13a7c16344cf76b855d5a75678
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2023-09-06 23:32:50 +0200

    fix condition to check whether make exists or not

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
-if ! command -v make &> /dev/null
-then
+command -v make >/dev/null 2>&1 || {
     echo "make tool is missing"
-    echo "if you don't want to install it you can check content of the ./docker/Makefile and run the corresponding command manually"
-    exit 1
-fi
+    echo "if you don't want to install it you can check content of the ./docker/Makefile and run the corresponding command manually"    
+    exit 1;
+}
 
 export SRC="$(pwd)"
 
